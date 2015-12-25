5e2cd9083dd7117d50b61802d946fd423ecdace1
Pace Willisson <pace@alum.mit.edu>
2015-12-25 15:33:29 -0500

    simple

--- website/receiver.php
+++ website/receiver.php
@@ -1,18 +1,11 @@
-<!--
-Copyright (C) 2014 Google Inc. All Rights Reserved.
+<?php
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+$anon_ok = 1;
+require_once ("common.php");
 
-     http://www.apache.org/licenses/LICENSE-2.0
+$ret = "";
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
--->
+$ret = '
 <!DOCTYPE html>
 <html>
   <head>
@@ -31,42 +24,42 @@ extra stuff
       window.onload = function() {
         cast.receiver.logger.setLevelValue(0);
         window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-        console.log('Starting Receiver Manager');
+        console.log("Starting Receiver Manager");
         
-        // handler for the 'ready' event
+        // handler for the "ready" event
         castReceiverManager.onReady = function(event) {
-          console.log('Received Ready event: ' + JSON.stringify(event.data));
+          console.log("Received Ready event: " + JSON.stringify(event.data));
           window.castReceiverManager.setApplicationState("Application status is ready...");
         };
         
-        // handler for 'senderconnected' event
+        // handler for "senderconnected" event
         castReceiverManager.onSenderConnected = function(event) {
-          console.log('Received Sender Connected event: ' + event.data);
+          console.log("Received Sender Connected event: " + event.data);
           console.log(window.castReceiverManager.getSender(event.data).userAgent);
         };
         
-        // handler for 'senderdisconnected' event
+        // handler for "senderdisconnected" event
         castReceiverManager.onSenderDisconnected = function(event) {
-          console.log('Received Sender Disconnected event: ' + event.data);
+          console.log("Received Sender Disconnected event: " + event.data);
           if (window.castReceiverManager.getSenders().length == 0) {
 	        window.close();
 	      }
         };
         
-        // handler for 'systemvolumechanged' event
+        // handler for "systemvolumechanged" event
         castReceiverManager.onSystemVolumeChanged = function(event) {
-          console.log('Received System Volume Changed event: ' + event.data['level'] + ' ' +
-              event.data['muted']);
+          console.log("Received System Volume Changed event: " + event.data["level"] + " " +
+              event.data["muted"]);
         };
 
         // create a CastMessageBus to handle messages for a custom namespace
         window.messageBus =
           window.castReceiverManager.getCastMessageBus(
-              'urn:x-cast:com.google.cast.sample.helloworld');
+              "urn:x-cast:com.google.cast.sample.helloworld");
 
         // handler for the CastMessageBus message event
         window.messageBus.onMessage = function(event) {
-          console.log('Message [' + event.senderId + ']: ' + event.data);
+          console.log("Message [" + event.senderId + "]: " + event.data);
           // display the message from the sender
           displayText(event.data);
           // inform all senders on the CastMessageBus of the incoming message event
@@ -76,7 +69,7 @@ extra stuff
 
         // initialize the CastReceiverManager with an application status message
         window.castReceiverManager.start({statusText: "Application is starting"});
-        console.log('Receiver Manager started');
+        console.log("Receiver Manager started");
       };
       
       // utility function to display the text message in the input field
@@ -88,3 +81,7 @@ extra stuff
     </script>
   </body>
 </html>
+
+';
+
+echo ($ret);
