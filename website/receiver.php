901efd2e249a3856e7c2d207c894157438ab508b
Pace Willisson <pace@alum.mit.edu>
2015-12-25 15:56:38 -0500

    recv

--- website/receiver.php
+++ website/receiver.php
@@ -5,83 +5,25 @@ require_once ("common.php");
 
 $ret = "";
 
-$ret = '
-<!DOCTYPE html>
-<html>
-  <head>
-    <style type="text/css">
-      div{
-	color:#ffffff;
-	}
-    </style>
-    <title>Cast Hello Text</title>
-  </head>
-  <body>
-extra stuff
-	<DIV id="message">Talk to me</DIV>
-    <script type="text/javascript" src="//www.gstatic.com/cast/sdk/libs/receiver/2.0.0/cast_receiver.js"></script>
-    <script type="text/javascript">
-      window.onload = function() {
-        cast.receiver.logger.setLevelValue(0);
-        window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-        console.log("Starting Receiver Manager");
-        
-        // handler for the "ready" event
-        castReceiverManager.onReady = function(event) {
-          console.log("Received Ready event: " + JSON.stringify(event.data));
-          window.castReceiverManager.setApplicationState("Application status is ready...");
-        };
-        
-        // handler for "senderconnected" event
-        castReceiverManager.onSenderConnected = function(event) {
-          console.log("Received Sender Connected event: " + event.data);
-          console.log(window.castReceiverManager.getSender(event.data).userAgent);
-        };
-        
-        // handler for "senderdisconnected" event
-        castReceiverManager.onSenderDisconnected = function(event) {
-          console.log("Received Sender Disconnected event: " + event.data);
-          if (window.castReceiverManager.getSenders().length == 0) {
-	        window.close();
-	      }
-        };
-        
-        // handler for "systemvolumechanged" event
-        castReceiverManager.onSystemVolumeChanged = function(event) {
-          console.log("Received System Volume Changed event: " + event.data["level"] + " " +
-              event.data["muted"]);
-        };
+$ret .= "<!DOCTYPE html>\n" 
+	."<html>\n"
+	."<head>\n"
+	."  <link rel='stylesheet' href='/rstyle.css' type='text/css' />\n"
+	."  <title>Cast Hello Text</title>\n"
+	."</head>\n"
+	."<body>\n"
+	;
 
-        // create a CastMessageBus to handle messages for a custom namespace
-        window.messageBus =
-          window.castReceiverManager.getCastMessageBus(
-              "urn:x-cast:com.google.cast.sample.helloworld");
+$ret .= "<h1>extra stuff</h1>\n";
+$ret .= "<div id='message'>Talk to me</div>\n";
 
-        // handler for the CastMessageBus message event
-        window.messageBus.onMessage = function(event) {
-          console.log("Message [" + event.senderId + "]: " + event.data);
-          // display the message from the sender
-          displayText(event.data);
-          // inform all senders on the CastMessageBus of the incoming message event
-          // sender message listener will be invoked
-          window.messageBus.send(event.senderId, event.data);
-        }
 
-        // initialize the CastReceiverManager with an application status message
-        window.castReceiverManager.start({statusText: "Application is starting"});
-        console.log("Receiver Manager started");
-      };
-      
-      // utility function to display the text message in the input field
-      function displayText(text) {
-        console.log(text);
-        document.getElementById("message").innerHTML=text;
-        window.castReceiverManager.setApplicationState(text);
-      };
-    </script>
-  </body>
-</html>
+$url = "//www.gstatic.com/cast/sdk/libs/receiver/2.0.0/cast_receiver.js";
+$ret .= sprintf ("<script type='text/javascript' src='%s'></script>\n", h($url));
 
-';
+$ret .= "<script type='text/javascript' src='receiver.js'></script>\n";
+
+$ret .= "</body>\n"
+	."</html>\n";
 
 echo ($ret);
